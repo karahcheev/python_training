a2515d38d4b0c9de1f1a9ed05fe4e6e40f2906cc
Kirill Rakhcheev <karahcheev@mail.ru>
2019-03-19 18:33:07 +0300

    Create group with katalon's helping

---
+++ create_group.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://localhost:444/addressbook/group.php")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_link_text("groups").click()
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys("test")
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys("logo")
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys("comment")
+        driver.find_element_by_name("submit").click()
+        driver.find_element_by_link_text("group page").click()
+
+    
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
