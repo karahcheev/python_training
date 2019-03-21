6642097f325a9629735811039ed25cd4d692b652
karahcheev <karahcheev@mail.ru>
2019-03-21 11:46:07 +0300

    Create group with katalon's helping

--- create_group.py
+++ create_group.py
@@ -2,39 +2,70 @@
 from selenium import webdriver
 import unittest
 
-class TestAddGroup(unittest.TestCase):
+
+class CreateGroup(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://localhost:444/addressbook/group.php")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("logo")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("comment")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
+    def open_home_page(self, wd):
+        """open home page"""
+        wd.get("http://localhost:444/addressbook/index.php")
 
-    
+    def login(self, wd, username="admin", password="secret"):
+        """login"""
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def fill_group_form(self, wd, name, header, footer):
+        """fill group form"""
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        """submit creation"""
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+                wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.fill_group_form(wd, name="group name", header="group header", footer="group footer")
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_epty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.fill_group_form(wd, name="", header="", footer="")
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
-        self.driver.quit()
-
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
