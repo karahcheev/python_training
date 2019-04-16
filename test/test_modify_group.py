311ae2aac040a9da5e35ab30490064eda943f6d5
karahcheev <karahcheev@mail.ru>
2019-04-16 20:16:38 +0300

    Checking len groups lust

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,5 +4,7 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="before deleting"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="new group name"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
