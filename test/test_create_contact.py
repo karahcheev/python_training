b6e0ed80fc99dc02fc6be5b55affb1ec7cad4a0a
karahcheev <karahcheev@mail.ru>
2019-04-26 14:08:57 +0300

    Not working eq for contact

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -3,28 +3,14 @@ from model.contact import Contact
 
 
 def test_create_contact(app):
-    contact = Contact(firstname="firs3", lastname="last4")
+    contact = Contact(firstname="firstname4", lastname="lastname4")
     old_contacts = app.contact.get_contact_list()
-    print(sorted(old_contacts, key=Contact.id_or_max))
+    print('\nOLD CONTACTS', sorted(old_contacts, key=Contact.id_or_max))
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
-    print(sorted(new_contacts, key=Contact.id_or_max))
+    print('\nNEW CONTACTS', sorted(new_contacts, key=Contact.id_or_max))
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    print("OLD CONTACT", sorted(old_contacts, key=Contact.id_or_max))
-    XXXX
+    print("\nADD CONTACTS", sorted(old_contacts, key=Contact.id_or_max))
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-
-
-    contacts = app.contact.get_contact_list()
-    print(contacts)
-
-
-    old_groups = app.group.get_group_list()
-    group = Group(name="group name", header="group header", footer="group footer")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_create_group.py
+++ test/test_create_group.py
@@ -5,10 +5,13 @@ from model.group import Group
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="group name", header="group header", footer="group footer")
+    print(old_groups)
     app.group.create(group)
     new_groups = app.group.get_group_list()
+    print(new_groups)
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
+    print(old_groups)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # def test_add_empty_group(app):
