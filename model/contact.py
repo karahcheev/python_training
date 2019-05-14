ebd82c22f9dd185987e5f5b9f773ad026025635d
Kirill Rakhcheev <karahcheev@mail.ru>
2019-05-14 10:03:16 +0300

    add test_emails

--- model/contact.py
+++ model/contact.py
@@ -8,7 +8,8 @@ class Contact:
                  homephone=None, mobilephone=None, workphone=None,
                  fax=None,email=None, email2=None, email3=None,
                  homepage=None, address2=None, secondaryphone=None,
-                 note=None, id=None, all_phones_from_home_page=None
+                 note=None, id=None, all_phones_from_home_page=None,
+                 all_emails_from_home_page=None
                  ):
         self.firstname = firstname
         self.middlename = middlename
@@ -30,6 +31,7 @@ class Contact:
         self.note = note
         self.id = id
         self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
         return "%s:%s %s" % (self.id, self.firstname, self.lastname)
--- test/test_phones.py
+++ test/test_phones.py
@@ -16,6 +16,13 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_home_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
+def test_email_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
