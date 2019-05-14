fca88f5602ff82bc230b3fb053ee7fd2ae428e6b
karahcheev <karahcheev@mail.ru>
2019-05-14 13:13:59 +0300

    Not working test_contact_info

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -10,6 +10,7 @@ def test_contact_info(app):
     if app.contact.count() == 0:
         app.contact.create(contact)
     index = randrange(len(app.contact.get_contact_list()))
+    print("\n### INDEX ###  ", index)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -3,9 +3,9 @@ from model.contact import Contact
 
 
 def test_create_contact(app):
-    contact = Contact(firstname="firstname4", lastname="lastname4", address="adress1",
-                      homephone="1111111", mobilephone="222222", workphone="333333",
-                      email="email1", email3="email3", address2="adress2", secondaryphone="444444")
+    contact = Contact(firstname="firstname3", lastname="lastname3", address="City3\nStreet3\nHouse3",
+                      email="email@mailbox.test", email2="email2@mailbox.test",
+                      homephone="+71112223344", workphone="(495)5553535", mobilephone="555 35 35")
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
