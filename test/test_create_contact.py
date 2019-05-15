98022a8f5fa6b27f076b0ddee6965d16e58e21e0
Kirill Rakhcheev <karahcheev@mail.ru>
2019-05-15 22:21:12 +0300

    add parameters in creating contact

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_create_contact(app):
-    contact = Contact(firstname="firstname4", lastname="lastname4", address="adress1",
-                      homephone="1111111", mobilephone="222222", workphone="333333",
-                      email="email1", email3="email3", address2="adress2", secondaryphone="444444")
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", address="", homephone="")] + [
+    Contact(firstname=random_string("firstname", 10),
+            lastname=random_string("lastname", 10),
+            address=random_string("adress1", 30),
+            homephone=random_string("111", 11),
+            mobilephone=random_string("222", 11),
+            workphone=random_string("333", 11),
+            email=random_string("email1", 15),
+            email3=random_string("email3", 10),
+            address2=random_string("adress2", 25),
+            secondaryphone=random_string("444", 15)
+
+    )
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_create_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
