c14d6bd484f18c44f1fd6384cf939408c165267c
karahcheev <karahcheev@mail.ru>
2019-05-16 17:45:38 +0300

    Not working Add download options from target.json and CL options

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -14,7 +14,7 @@ testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10),
           header=random_string("header", 20),
           footer=random_string("footer", 20))
-    for i in range(5)
+    for i in range(1)
 ]
 
 
